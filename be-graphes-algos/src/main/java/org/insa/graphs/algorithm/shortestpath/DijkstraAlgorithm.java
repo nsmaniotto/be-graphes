ed4ba11b2046ae9d35bf1642e8bd52fb83833d76
nsmaniotto <smaniotto.nathan@gmail.com>
2020-03-30 11:42:45 +0200

    Implémentation classes Label et DijkstraAlgorithm

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -10,7 +22,106 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        BinaryHeap<Label> binaryHeap = new BinaryHeap<Label>() ;
+        ArrayList<Arc> arcPath = new ArrayList<Arc>() ;
+        
+        final Graph graph = data.getGraph();
+        final List<Node> nodes = graph.getNodes();
+        final int origin = data.getOrigin().getId();
+        final int destination = data.getDestination().getId();
+        
+        // Notify observers about the first event (origin processed).
+        notifyOriginProcessed(data.getOrigin());
+        
+        /* INITIALISATION */
+        Label[] labelTab = new Label[nodes.size()];
+        
+        for(Node node: nodes) { // For every node of the graph
+        	labelTab[node.getId()] = new Label(node) ;
+        }
+        
+        labelTab[origin].setCost(0.); // Cost(s) <- 0
+        
+        binaryHeap.insert(labelTab[origin]); // Insert(s, heap)
+        
+        
+        /* ITERATIONS */
+        
+        while(labelTab[destination].isNotMarked() && !binaryHeap.isEmpty()) { // While nodes are not marked and there are nodes to mark
+        	
+        	/* Finding and deleting the min label of our binary heap */
+        	Label x = binaryHeap.findMin();
+        	binaryHeap.deleteMin();
+        	
+        	/* Marking our current label */
+        	x.setMarked(true);
+        	labelTab[x.getNode().getId()].setMarked(true);
+        	
+        	// Notify all observers that a node has been marked, i.e. its final value has been set.
+            notifyNodeMarked(x.getNode());
+        	
+        	/* For every successor of x */
+        	for(Arc arcToY: x.getNode().getSuccessors()) {
+        		
+        		Node y = arcToY.getDestination(); // get current Y
+        		
+        		if(labelTab[y.getId()].isNotMarked()) {
+        			
+        			double currentCost = labelTab[y.getId()].getCost();
+        			double potentialNewCost = x.getCost() + this.data.getCost(arcToY);
+        			
+        			// Notify all observers that a node has been reached for the first time.
+        			notifyNodeReached(arcToY.getDestination());
+        			
+        			if(potentialNewCost < currentCost) {
+        				
+        				/* Updates values*/
+        				labelTab[y.getId()].setCost(potentialNewCost);
+                        labelTab[y.getId()].setArcFromFather(arcToY);
+                        
+                        /* Updating our binary heap */
+                        try {
+                        	
+                            binaryHeap.remove(labelTab[y.getId()]);
+                            
+                        } catch(Exception e) {
+                        	
+                        }
+
+                        binaryHeap.insert(labelTab[y.getId()]);
+                        
+        			}
+        			
+        		}
+        		
+        	}
+        	
+        }
+        
+        // The destination has been found, notify the observers.
+        notifyDestinationReached(data.getDestination());
+        
+        /* Generate the path from the previous selected arcs, starting from the end of the path */
+        Label currentLabel = labelTab[destination];
+        Arc currentArcFromFather = currentLabel.getArcFromFather();
+        
+        while(currentArcFromFather != null) {
+        	
+        	arcPath.add(currentArcFromFather);
+        	
+        	currentLabel = labelTab[currentArcFromFather.getOrigin().getId()];
+        	
+        	currentArcFromFather = currentLabel.getArcFromFather();
+        	
+        }
+        
+        /* Reverse our list of arcs since we started from the end of the path */
+        Collections.reverse(arcPath);
+        
+        /* Generate the path solution from our list of arcs */
+        solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcPath));
+        
         return solution;
     }
 
---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,101 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Arc;
+
+/**
+ * Label associated with a node.
+ * 
+ * Must implement Comparable to be used in a BinaryHeap
+ * 
+ * @author Nathan Smaniotto.
+ */
+public class Label implements Comparable<Label> {
+	
+	// sommet associé à ce label
+	private Node currentNode;
+	
+	// vrai lorsque le coût min de ce sommet est définitivement connu par l'algorithme
+	private boolean marked;
+	
+	// valeur courante du plus court chemin depuis l'origine vers le sommet
+	private double cost;
+	
+	// correspond à l'arc du sommet précédent sur le chemin correspondant au plus court chemin courant
+	private Arc arcFromFather;
+	
+	/**
+     * Construct a new label.
+     * 
+     * @param node Node associated with this label.
+     */
+    public Label(Node node) {
+        this.currentNode = node;
+        this.marked = false;
+        this.cost = Double.POSITIVE_INFINITY;
+        this.arcFromFather = null;
+    }
+
+    /**
+     * @return Node associated with this label.
+     */
+    public Node getNode() {
+    	return this.currentNode;
+    }
+
+    /**
+     * @param status of the label.
+     */
+    public void setMarked(boolean marked) {
+    	this.marked = marked;
+    }
+
+    /**
+     * @return boolean true when marked.
+     */
+    public boolean isMarked() {
+    	return this.marked;
+    }
+
+    /**
+     * @return boolean true when not marked.
+     */
+    public boolean isNotMarked() {
+    	return !this.marked;
+    }
+
+    /**
+     * @param cost value of the cost to this node.
+     */
+    public void setCost(double cost) {
+    	this.cost = cost;
+    }
+
+    /**
+     * @return current value of the cost to this node.
+     */
+    public double getCost() {
+    	return this.cost;
+    }
+
+    /**
+     * @param arcFromFather new value to the arc.
+     */
+    public void setArcFromFather(Arc arcFromFather) {
+    	this.arcFromFather = arcFromFather;
+    }
+
+
+    /**
+     * @return arc from the father.
+     */
+    public Arc getArcFromFather() {
+    	return this.arcFromFather;
+    }
+    
+    @Override
+    public int compareTo(Label label) {
+        return Integer.compare(this.getNode().getId(), label.getNode().getId()) ;
+    }
+
+}
